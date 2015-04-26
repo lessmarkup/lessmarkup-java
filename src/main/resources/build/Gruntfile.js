4860fb580125dfebe81fe6f5a093ff588337230d
oleg-gordeev <oleg@gordeev.cz>
2015-04-26 03:17:12 +0300

    Updated configuration to remove grunt-contrib-sass

--- src/main/resources/build/Gruntfile.js
+++ src/main/resources/build/Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-tsd');
